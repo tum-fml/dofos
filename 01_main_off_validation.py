de8610a7895285803365c2936ecf8ba9ad5c8144
Filippos Chamoulias <filippos.chamoulias@tum.de>
2021-06-08 09:18:50 +0000

    Update 01_main_off_validation.py

--- 01_main_off_validation.py
+++ 01_main_off_validation.py
@@ -30,6 +30,7 @@ def main():
     print("--------------------------------------------")
 
     # Validierung
+    # Insert the path of the validation set
     alles = pd.read_csv("C:/Users/Chamou/LRZ Sync+Share/01_Paper/04_Daten_Validierung/datalog4.txt", sep=' ', names=['x', 'y', 'z']
                      , usecols=[0, 1, 2])
 
@@ -39,26 +40,21 @@ def main():
     df = c.clean_data(alles)
 
     df = alles
-    # df = alles.iloc[26400:28400]
 
-    print(len(df))
-
+    # Preparing Plot for later vizualisation 
     fig, ax = plt.subplots()
     ax.plot(df, color='k')
 
-
+    # The same features must be used as for training
     features = [0, 4, 5]
-    # Trainingsdaten
-    # df = pd.read_csv("C:/Users/Chamou/PycharmProjects/01_Mustererkennung_Beschleunigungssensor/Datenbank_Betriebszustände/"
-    #                  "03_aufschlagen_zinke_untergrund/sample_pattern13.txt", sep=' ', names=['x', 'y', 'z'], usecols=[0, 1, 2])
-    # print(len(alles))
 
+    # Load ML Model
     with open('R_3conditions.pkl', 'rb') as m:
         classifier = joblib.load(m)
 
 
+    # Select window size
     non_overlapping = 50
-
 
     x = non_overlapping
     steps = non_overlapping
@@ -67,23 +63,15 @@ def main():
     start = 0
     stop = len(df)
 
-    # start = 26400
-    # stop = 28400
-    starttime = perf_counter()
 
     for i in range(start, stop, steps):
 
-        # starttime = perf_counter()
-
         window = df.iloc[0+i:x+i]
-
-        # window = df.iloc[57600:59400]
-
 
         feature_matrix = np.zeros(3 * (len(features)))
         extraction = c.features(len(features), feature_matrix, 1)
 
-        # df = c.med_filter(df, 35)
+        # df = c.med_filter(df, 3)
         # df = c.clean_data(df)
 
         l = 0
@@ -91,10 +79,9 @@ def main():
         if 0 in features:
             extraction.peak_to_peak(window, n, l)
             l += 3
-
-        # if 1 in features:
-        #     extraction.euclidean_Dist(df, n, l)
-        #     l += 3
+        if 1 in features:
+            extraction.euclidean_Dist(df, n, l)
+            l += 3
         if 2 in features:
             extraction.fft(window, n, l)
             l += 3
@@ -120,9 +107,6 @@ def main():
         condition = classifier.predict(feature_matrix.reshape(1,-1))
         # print("Condition is: ", condition, " @ ", "i = ", i)
 
-        # stoptime = perf_counter()
-        #
-        # print("Elapsed time:", stoptime - starttime)
 
         if condition == 0:
             color = 'blue'
@@ -148,7 +132,6 @@ def main():
         if condition == 7:
             color = 'silver'
             a = 0.3
-        # ax[0].axvspan(0 + i, x + i, alpha=a, color=color)
         ax.axvspan(0 + i,x + i, alpha=a, color=color)
     fontzize = 14
     plt.xlabel('Datapoints', fontsize=fontzize)
@@ -158,11 +141,6 @@ def main():
     plt.yticks(fontsize=fontzize)
     plt.xticks(fontsize=fontzize)
     plt.show()
-
-    stoptime = perf_counter()
-
-    print("Elapsed time:", stoptime - starttime)
-    print(i)
 
 if __name__ == "__main__":
     main()
